3a6c7306ffe627c98744efcbe95d683d2cbd6a01
Nitin Mittal <nitinmittal44066@gmail.com>
2024-07-23 19:05:34 +0400

    fix: image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,12 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta property="og:image" content="https://deq-labs-social-1.s3.amazonaws.com/Deq+Twitter+Cover.png.jpg" />
+        <meta property="og:image" content="https://deq-labs-social-1.s3.amazonaws.com/Deq+Twitter+Cover.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
 
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="https://deq-labs-social-1.s3.amazonaws.com/Deq+Twitter+Cover.png.jpg" />
+        <meta name="twitter:image" content="https://deq-labs-social-1.s3.amazonaws.com/Deq+Twitter+Cover.png" />
       </Head>
       <body>
         <Main />
