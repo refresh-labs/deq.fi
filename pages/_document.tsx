47bdafc21911e92f2a792149218f6bf5262cdce3
Ankit Boghra <ankitboghra@gmail.com>
2024-07-23 19:00:54 +0400

    feat: og images try 10
    Signed-off-by: Ankit Boghra <ankitboghra@gmail.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,12 +4,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta property="og:image" content="https://deq-labs-social-1.s3.amazonaws.com/Deq+Twitter+Cover.png.jpg" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="https://deq-labs-social-1.s3.amazonaws.com/Deq+Twitter+Cover.png.jpg" />
+      <meta property="og:title" content="deq.fi">
+      <meta property="og:image:type" content="image/png"></meta>
+      <meta property="og:image:width" content="2400"></meta>
+      <meta property="og:image:height" content="1260"></meta>
+      <meta property="og:image" content="https://deq-labs-social-1.s3.amazonaws.com/Deq+Twitter+Cover.png"></meta>
+      <meta name="twitter:card" content="summary_large_image"></meta>
+      <meta name="twitter:title" content="deq.fi"></meta>
+      <meta name="twitter:image:type" content="image/png"></meta>
+      <meta name="twitter:image:width" content="2400"></meta>
+      <meta name="twitter:image:height" content="1260"></meta>
+      <meta name="twitter:image" content="https://deq-labs-social-1.s3.amazonaws.com/Deq+Twitter+Cover.png"></meta>
       </Head>
       <body>
         <Main />
