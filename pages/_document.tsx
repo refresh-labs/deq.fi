8968653f1c48afb0333c99b1cffe9a64b867a080
Ankit Boghra <ankitboghra@gmail.com>
2024-07-23 19:16:38 +0400

    feat: og images last try
    Signed-off-by: Ankit Boghra <ankitboghra@gmail.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,13 +8,13 @@ export default function Document() {
       <meta property="og:image:type" content="image/png"></meta>
       <meta property="og:image:width" content="2400"></meta>
       <meta property="og:image:height" content="1260"></meta>
-      <meta property="og:image" content="http://deq-labs-social-1.s3.amazonaws.com/Deq+Twitter+Cover.png"></meta>
+      <meta property="og:image" content="https://res.cloudinary.com/dczghbro7/image/upload/v1721746189/og-image_mzcne9.png"></meta>
       <meta name="twitter:card" content="summary_large_image"></meta>
       <meta name="twitter:title" content="deq.fi"></meta>
       <meta name="twitter:image:type" content="image/png"></meta>
       <meta name="twitter:image:width" content="2400"></meta>
       <meta name="twitter:image:height" content="1260"></meta>
-      <meta name="twitter:image" content="http://deq-labs-social-1.s3.amazonaws.com/Deq+Twitter+Cover.png"></meta>
+      <meta name="twitter:image" content="https://res.cloudinary.com/dczghbro7/image/upload/v1721746189/og-image_mzcne9.png"></meta>
       </Head>
       <body>
         <Main />
